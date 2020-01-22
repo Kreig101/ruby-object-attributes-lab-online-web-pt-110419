59e679f8d24dcc83321217e280d6cfffceb53c4a
Craig Storer <craigstorer@gmail.com>
2020-01-22 19:15:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,18 @@
 class Person
-  def 
+   def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end 
   
+  def job=(job_name)
+    @job = job_name
+  end
+  
+  def job
+    @job
+  end
   
 end
